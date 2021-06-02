1d8be3620da39264705de12025bae0a855921237
joeDohne <j03.d0hne@gmail.com>
2021-06-02 04:10:16 +0000

    msg

--- monte_carlo.c
+++ monte_carlo.c
@@ -1,10 +1,9 @@
-#define _XOPEN_SOURCE
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
-#define M_PI 3.14159265358979323846
+// #define M_PI 3.14159265358979323846
 
 float mc_pi(int);
 
